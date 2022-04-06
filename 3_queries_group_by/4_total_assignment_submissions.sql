b2662b042b08469813ad23af7c0795c1c5ab7268
Eugene Helland <eugenehelland@mail.com>
2022-04-06 15:06:33 -0600

    Add and populate teacher and assistance_requests tables.

--- 3_queries_group_by/4_total_assignment_submissions.sql
+++ 3_queries_group_by/4_total_assignment_submissions.sql
@@ -1,6 +1,6 @@
-SELECT cohorts.name AS cohort, count(assignment_submissions.*) AS total_submissions
-FROM cohorts
-INNER JOIN students ON cohort.id = cohort_id
-INNER JOIN assignment_submissions ON student.id = student_id
-GROUP BY cohort
-ORDER BY total_submissions;
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
